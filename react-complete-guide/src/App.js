99cbc16de9fd147164f5dea88bb52bedc025069c
MariamJamal <mariamjamal32@gmail.com>
2018-09-25 08:04:20 +0500

    first functional component

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person'
 
 class App extends Component {
+  something = 'Mariam'
   render() {
     return (
       <div className="App">
         <h1>Hi, I am a react App! </h1>
+        <p>It is really working!! </p>
+        <Person name="Max" age="28"/>
+        <Person name={this.something} age="25"> I am a girl. </Person>
+        
       </div>
     );
   }
